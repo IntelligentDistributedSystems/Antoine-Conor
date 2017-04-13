e1d8c876cae2a1093d074672287f977caec580cb
Evoliofly <antoine.murat@protonmail.com>
2017-04-13 19:32:52 +0300

    Build, debug and run scripts.

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Ant has to be installed!!! : sudo apt-get install ant
+ant -f bin/build.xml jar
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# JDK has to be installed!!! : sudo apt-get install openjdk-8-jdk
-# /path/to/java -classpath /path/to/ant-launcher-1.9.7.jar org.apache.tools.ant.launch.Launcher -e -f /path/to/build.xml 
-java -classpath bin/libs/ant-launcher-1.9.7.jar org.apache.tools.ant.launch.Launcher -e -f bin/build.xml
+# You have to run ./build.sh before running this file.
+# Java has to be installed.
+java -jar guardianPatrol.jar
